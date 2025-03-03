0ad4a2b01ed73cfd91d529f688ebaaa9b2fce28d
Raffael E <raffa_e@web.de>
2025-03-03 11:55:31 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# Best Buy
+
+Simple CLI program to manage a small store by adding products and creating orders.
---
+++ src/product.py
@@ -0,0 +1,47 @@
+class Product:
+    def __init__(self, name, *, price, quantity):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+    def get_quantity(self):
+        return self.quantity
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+
+    def is_active(self):
+        return self.active
+
+    def activate(self):
+        self.active = True
+
+    def deactivate(self):
+        self.active = False
+
+    def show(self):
+        return f"{self.name}, Price: {self.price}, Quantity: {self.quantity}"
+
+    def buy(self, quantity):
+        self.quantity -= quantity
+        return self.price * quantity
+
+
+def main():
+    bose = Product("Bose QuietComfort Earbuds", price=250, quantity=500)
+    mac = Product("MacBook Air M2", price=1450, quantity=100)
+
+    print(bose.buy(50))
+    print(mac.buy(100))
+    print(mac.is_active())
+
+    print(bose.show())
+    print(mac.show())
+
+    bose.set_quantity(1000)
+    print(bose.show())
+
+
+if __name__ == '__main__':
+    main()
